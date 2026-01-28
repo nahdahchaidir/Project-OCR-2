8404a298652b77b1e5e6be07ec0edbd80634730b
nahdahchaidir <nafaucha@gmail.com>
2026-01-28 14:18:26 +0800

    hapusss

--- api/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Sesuaikan dengan setting Laragon kamu
-define('DB_HOST', '127.0.0.1');
-define('DB_NAME', 'db_pln');
-define('DB_USER', 'root');
-define('DB_PASS', ''); // default Laragon biasanya kosong
